ec16cba5feaa890f6eab076d154f87b8ec5fd4f7
kkkkea <447872721@qq.com>
2026-06-29 23:44:35 +0800

    update

--- evaluations/image_folder_to_npz.py
+++ evaluations/image_folder_to_npz.py
@@ -6,10 +6,25 @@ import numpy as np
 from PIL import Image
 from tqdm.auto import tqdm
 
-from guided_diffusion.image_datasets import center_crop_arr
-
 
 IMG_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif"}
+
+
+def center_crop_arr(pil_image, image_size):
+    while min(*pil_image.size) >= 2 * image_size:
+        pil_image = pil_image.resize(
+            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
+        )
+
+    scale = image_size / min(*pil_image.size)
+    pil_image = pil_image.resize(
+        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
+    )
+
+    arr = np.array(pil_image)
+    crop_y = (arr.shape[0] - image_size) // 2
+    crop_x = (arr.shape[1] - image_size) // 2
+    return arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size]
 
 
 def list_images(data_dir):
